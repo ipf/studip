8bdaac8d6821006d88fc2b154a53512412cde53e
Till Glöggler <tgloeggl@uos.de>
2008-08-25 11:08:11 +0000

    fixes #224 for 1.7
    git-svn-id: svn://develop.studip.de/studip/branches/1.7@10245 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- lib/raumzeit/CycleData.class.php
+++ lib/raumzeit/CycleData.class.php
@@ -174,8 +174,7 @@ class CycleData {
 						}
 					}
 				}
-				SingleDateDB::deleteSingleDate($termin->termin_id, $termin->ex_termin);
-				//$termin->delete();
+				$termin->delete();
 			}
 		}
 		return TRUE;
