8b1abf44cdfacb6d82b0e178b138250f72772625
André Noack <noack@data-quest.de>
2008-09-02 15:18:44 +0000

    Merged revisions 10284-10308 via svnmerge from svn://develop.studip.de/studip/trunk
    ........
      r10294 | elmar | 2008-09-01 12:50:16 +0200 (Mo, 01 Sep 2008) | 2 lines
      
      fix ambiguous SQL error (regression from changeset [10021]), re #155
    ........
      r10303 | ludwig | 2008-09-02 15:03:32 +0200 (Di, 02 Sep 2008) | 2 lines
      
       * Fix stundenplan (ende_zeit sollte größer sein als begin)
    ........
      r10308 | anoack | 2008-09-02 17:17:23 +0200 (Di, 02 Sep 2008) | 1 line
      
      bugfix typo, re #155
    ........
    
    
    git-svn-id: svn://develop.studip.de/studip/branches/1.8@10309 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- lib/resources/resourcesFunc.inc.php
+++ lib/resources/resourcesFunc.inc.php
@@ -425,13 +425,13 @@ function getMyRoomRequests($user_id = '') {
 			while ($db2->next_record()) {
 				$requests [$db2->f("request_id")]["my_res"] = TRUE;
 				$requests [$db2->f("request_id")]["closed"] = $db2->f("closed");
-				$requests [$db2->f("request_id")]["have_times"] = $db->f('rr_termin_id') ? ($db->f("tt_termin_id") == $db->f('rr_termin_id')) : $db->f("anzahl_termine");
+				$requests [$db2->f("request_id")]["have_times"] = $db2->f('rr_termin_id') ? ($db2->f("tt_termin_id") == $db2->f('rr_termin_id')) : $db2->f("anzahl_termine");
 			}
 		}
 		if (sizeof($my_sems)) {
 			$in_seminar_id =  "('".join("','",array_keys($my_sems))."')";
 			$query_sem = sprintf("SELECT request_id, closed, tt.termin_id as tt_termin_id, rr.termin_id as rr_termin_id, 
-								COUNT(IF(date_typ IN ".getPresenceTypeClause(). ",t.termin_id,NULL)) as anzahl_termine
+								COUNT(IF(t.date_typ IN ".getPresenceTypeClause(). ",t.termin_id,NULL)) as anzahl_termine
 								FROM resources_requests rr
 								INNER JOIN seminare s USING(seminar_id)
 								LEFT JOIN termine tt ON (tt.termin_id = rr.termin_id)
