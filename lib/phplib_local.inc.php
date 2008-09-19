8ceedafe1611583d9cebca28cdd001b2a70566b4
Jens Schmelzer <jens.schmelzer@fh-jena.de>
2008-09-19 22:19:25 +0000

    Vorbereitung Release 1.7.1
    git-svn-id: svn://develop.studip.de/studip/branches/1.7@10495 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- lib/phplib_local.inc.php
+++ lib/phplib_local.inc.php
@@ -84,7 +84,7 @@ $GLOBALS['_fullname_sql']['no_title_rev'] = "CONCAT(Nachname ,', ', Vorname)";
 $GLOBALS['_fullname_sql']['no_title_short'] = "CONCAT(Nachname,', ',UCASE(LEFT(TRIM(Vorname),1)),'.')";
 $GLOBALS['_fullname_sql']['no_title_motto'] = "CONCAT(Vorname ,' ', Nachname,IF(motto!='',CONCAT(', ',motto),''))";
 //software version - please leave it as it is!
-$SOFTWARE_VERSION = '1.7.0';
+$SOFTWARE_VERSION = '1.7.1';
 
 /*classes for database access
 ----------------------------------------------------------------
