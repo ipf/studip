374cb056dbe56b65970578016f5266b3323431ec
André Noack <noack@data-quest.de>
2009-11-12 16:40:28 +0000

    Datenbankdumps config Einstellungen korrigiert für 1.10
    git-svn-id: svn://develop.studip.de/studip/branches/1.10@13860 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- db/migrations/44_add_switch_to_preselect_semester.php
+++ db/migrations/44_add_switch_to_preselect_semester.php
@@ -9,7 +9,7 @@ class AddSwitchToPreselectSemester extends Migration {
 		 		"`value`, `is_default`, `type`, `range`, `section`, `position`, `mkdate`, `chdate`," .
 		 		"`description`, `comment`, `message_template`) VALUES " .
 		 		"('06d703c3de37cdae942c66e18f7dcd02', '', 'ASSI_SEMESTER_PRESELECT', " .
-		 		"'TRUE', 0, 'boolean', 'global', '', 0, ".time().",". time().
+		 		"'1', 1, 'boolean', 'global', '', 0, ".time().",". time().
 				", 'Wenn ausgeschaltet wird im admin_seminare_assi beim" .
 				"Erstellen einer Veranstaltung als Semester bitte auswählen " .
 				"angezeigt und nicht das voreingestellte Semester.', '', '')");
