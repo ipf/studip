c7c6d621d53495e8179dfa058583013c2d00ffb6
Elmar Ludwig <elmar.ludwig@uos.de>
2010-02-12 15:03:51 +0000

    Merged revisions 13638,13641,13687,14439,14497 via svnmerge from svn://develop.studip.de/studip/trunk
    ........
      r13638 | sziemke | 2009-10-16 12:15:51 +0200 (Fri, 16 Oct 2009) | 3 lines
      
      fixes #802 for trunk
      
      modified error message and added break to cancel message output
    ........
      r13641 | sziemke | 2009-10-16 13:23:31 +0200 (Fri, 16 Oct 2009) | 3 lines
      
      fixes #802
      
      problem fixed. Only generate error no days in timeslot when no other errors occured
    ........
      r13687 | aklassen | 2009-10-26 11:40:28 +0100 (Mon, 26 Oct 2009) | 2 lines
      
      add jbickman's changes fixes #792
    ........
      r14439 | tgloeggl | 2010-01-27 16:02:18 +0100 (Wed, 27 Jan 2010) | 1 line
      
      fixes #579
    ........
      r14497 | jbickman | 2010-02-05 16:10:08 +0100 (Fri, 05 Feb 2010) | 2 lines
      
      fixes #693
    ........
    
    
    git-svn-id: svn://develop.studip.de/studip/branches/1.10@14557 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- lib/blockveranstaltungs_assistent.inc.php
+++ lib/blockveranstaltungs_assistent.inc.php
@@ -134,7 +134,7 @@ function create_block_schedule_dates($seminar_id, $form_data)
 			if ($start_time==$end_time)	{
 				$errors[] = "Start- und Endzeitpunkt sind gleich!";
 			} else if ($start_time>$end_time)	{
-				$errors[] = "Startzeitpunkt liegt vor Endzeitpunkt!";
+				$errors[] = "Startzeitpunkt liegt nach Endzeitpunkt!";
 			}
 		}
 
@@ -147,7 +147,7 @@ function create_block_schedule_dates($seminar_id, $form_data)
 
 		if ($start_time > $absolute_end_time)
 		{
-			$errors[] = "Startdatum liegt vor Enddatum!";
+			$errors[] = "Startdatum liegt nach Enddatum!";
 		}
 
 
@@ -189,12 +189,13 @@ function create_block_schedule_dates($seminar_id, $form_data)
 				$tmp_start_time = strtotime("+1 day "." ".$form_data["start_hour"].":".$form_data["start_minute"], $tmp_start_time);
 				$tmp_end_time = strtotime("+1 day ".$form_data["end_hour"].":".$form_data["end_minute"], $tmp_end_time);
 			}
+			if (!isset($schedule_dates)) {
+					$errors[] = $messages['no_days_in_timeslot'];
+			}
 		}
 	}
 
-	if (!isset($schedule_dates)) {
-		$errors[] = $messages['no_days_in_timeslot'];
-	}
+
 
 	//echo "<pre>".print_r($GLOBALS,true)."<pre>";;
 	if (!isset($form_data["block_submit_x"]) || $errors != null)
--- public/admin_seminare1.php
+++ public/admin_seminare1.php
@@ -87,7 +87,7 @@ function checkdata(command){
 
  if (checked) {
    document.details.method = "post";
-   document.details.action = "<?php echo URLHelper::getLink($PHP_SELF) ?>";
+   document.details.action = "<?php echo URLHelper::getURL() ?>";
    document.details.submit();
  }
  return checked;
@@ -100,7 +100,7 @@ function checkdata_without_bereich(command){
  	checked = false;
  if (checked) {
    document.details.method = "post";
-   document.details.action = "<?php echo URLHelper::getLink($PHP_SELF) ?>";
+   document.details.action = "<?php echo URLHelper::getURL() ?>";
    document.details.submit();
  }
  return checked;
@@ -178,7 +178,7 @@ function get_dozent_data($s_id, $_fullname_sql, $locked = false, $lock_text = ''
 			  $out[]= "<td>";
 			$href = "?delete_doz=".$db->f("username"). "&s_id=".$s_id."#anker";
 			$img_src = "images/trash.gif";
-  			$out[] = "<a href='".URLHelper::getLink($PHP_SELF.$href)."'>";
+  			$out[] = "<a href='".URLHelper::getLink($href)."'>";
   			$out[] = "<img src='{$GLOBALS['ASSETS_URL']}{$img_src}' border='0'>";
   			$out[] = "</a>";
   			$out[]= "</td>";
@@ -191,7 +191,7 @@ function get_dozent_data($s_id, $_fullname_sql, $locked = false, $lock_text = ''
         {
 						$href = "?moveup_doz=".$db->f("username"). "&s_id=".$s_id."&foo=".time()."#anker";
             $img_src = "images/move_up.gif";
-  						$out[] = "<a href='".URLHelper::getLink($PHP_SELF.$href)."'>";
+  						$out[] = "<a href='".URLHelper::getLink($href)."'>";
   						$out[] = "<img src='{$GLOBALS['ASSETS_URL']}{$img_src}' border='0'>";
   						$out[] = "</a>";
         }
@@ -202,7 +202,7 @@ function get_dozent_data($s_id, $_fullname_sql, $locked = false, $lock_text = ''
           {
 						$href = "?movedown_doz=".$db->f("username"). "&s_id=".$s_id."&foo=".time()."#anker";
 						$img_src = "images/move_down.gif";
-  						$out[] = "<a href='".URLHelper::getLink($PHP_SELF.$href)."'>";
+  						$out[] = "<a href='".URLHelper::getLink($href)."'>";
   						$out[] = "<img src='{$GLOBALS['ASSETS_URL']}{$img_src}' border='0'>";
   						$out[] = "</a>";
 				}
@@ -287,7 +287,7 @@ function get_tutor_data($s_id, $_fullname_sql, $locked = false, $lock_text = '')
 			$href =   "?delete_tut=".$db->f("username"). "&s_id=".$s_id."#anker";
 			$img_src = "images/trash.gif";
 
-  			$out[] = "<a href='".URLHelper::getLink($PHP_SELF.$href)."'>";
+  			$out[] = "<a href='".URLHelper::getLink($href)."'>";
   			$out[] = "<img src='{$GLOBALS['ASSETS_URL']}{$img_src}' border='0'>";
   			$out[] = "</a>";
 
@@ -302,7 +302,7 @@ function get_tutor_data($s_id, $_fullname_sql, $locked = false, $lock_text = '')
 						$href = "?moveup_tut=".$db->f("username"). "&s_id=".$s_id."&foo=".time()."#anker";
 						$img_src = "images/move_up.gif";
 
-  						$out[] = "<a href='".URLHelper::getLink(PHP_SELF.$href)."'>";
+  						$out[] = "<a href='".URLHelper::getLink($href)."'>";
   						$out[] = "<img src='{$GLOBALS['ASSETS_URL']}{$img_src}' border='0'>";
   						$out[] = "</a>";
         }
@@ -314,7 +314,7 @@ function get_tutor_data($s_id, $_fullname_sql, $locked = false, $lock_text = '')
 					$href = "?movedown_tut=".$db->f("username"). "&s_id=".$s_id."&foo=".time()."#anker";
 					$img_src = "images/move_down.gif";
 
-  					$out[] = "<a href='".URLHelper::getLink($PHP_SELF.$href)."'>";
+  					$out[] = "<a href='".URLHelper::getLink($href)."'>";
   					$out[] = "<img src='{$GLOBALS['ASSETS_URL']}{$img_src}' border='0'>";
   					$out[] = "</a>";
 				}
@@ -902,7 +902,7 @@ if (($s_id) && (auth_check())) {
 	<?
 
 	// ab hier Anzeigeroutinen ///////////////////////////////////////////////
-	echo "<form name=\"details\" method=\"post\" action=\"".URLHelper::getLink($PHP_SELF."#anker")."\">";
+	echo "<form name=\"details\" method=\"post\" action=\"".URLHelper::getLink("#anker")."\">";
 	?>
 	<table border=0 align="center" cellspacing=0 cellpadding=2 width="99%">
 		<input type="hidden" name="s_id"   value="<?php $db->p("Seminar_id") ?>">
