434db70f751fb6b82b7bfea5909befeb6213c3b2
André Noack <noack@data-quest.de>
2010-05-26 10:48:39 +0000

    1.11 Universität Rostock
    git-svn-id: svn://develop.studip.de/studip/branches/svn:@15855 c5b85792-5220-0410-ad23-ed5b6b8ad999

---
+++ develop.studip.de/studip/local-branches/uro/public/datenschutz.php
@@ -0,0 +1,35 @@
+<?php
+# Lifter007: TODO
+
+/**
+ * datenschutz.php
+ *
+ * privacy guidelines for Stud.IP
+ *
+ * PHP version 5
+ *
+ * @author 		Elmar Ludwig
+ * @author  	Michael Riehemann <michael.riehemann@uni-oldenburg.de>
+ * @copyright 	2009 Stud.IP
+ * @license 	http://www.gnu.org/licenses/gpl.html GPL Licence 3
+ * @package 	studip_core
+ * @access 		public
+ */
+
+page_open(array(
+	'sess' => 'Seminar_Session',
+	'auth' => 'Seminar_Default_Auth',
+	'perm' => 'Seminar_Perm',
+	'user' => 'Seminar_User'
+));
+
+// set up user session
+include 'lib/seminar_open.php';
+
+$CURRENT_PAGE = _('Erläuterungen zum Datenschutz');
+
+$template = $template_factory->open('privacy');
+$template->set_layout('layouts/base_without_infobox');
+
+echo $template->render();
+?>
---
+++ develop.studip.de/studip/local-branches/uro/templates/infobox/infobox_generic_content.php
@@ -0,0 +1,52 @@
+<?
+//
+// Standard Infobox-Layout as of july 2007 used by the method print_infobox() in visual.inc.php
+// WARNING : This type of infobox is deprecated and will be subject to changes! 
+//           It's provided for compatibility reasons only. 
+//
+?>
+
+<table align="center" width="250" cellpadding="0" cellspacing="0">
+
+    <? if ($picture) : ?>
+
+      <tr>
+        <td class="blank">
+		  <?= Assets::img($picture) ?>
+        </td>
+      </tr>
+
+    <? endif ?>
+
+    <tr>
+      <td class="infoboxrahmen">
+        <table cellpadding="4" cellspacing="0">
+
+          <? foreach ($content as $category) : ?>
+              <tr>
+                <td class="infobox" colspan="2">
+                  <font size="-1"><b><?= $category["kategorie"] ?></b></font>
+                </td>
+              </tr>
+
+            <? if (isset($category['eintrag'])) : ?>
+              <? foreach ($category['eintrag'] as $item) : ?>
+
+                <tr>
+                  <td class="infobox" width="1%" align="center" valign="top">
+                    <?= Assets::img($item['icon']) ?>
+                  </td>
+                  <td class="infobox" width="99%">
+                    <?= $item["text"] ?>
+                  </td>
+                </tr>
+
+              <? endforeach ?>
+
+            <? endif ?>
+          <? endforeach ?>
+
+        </table>
+      </td>
+    </tr>
+  </table>
