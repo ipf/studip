8ec80612e4401d7cc0ea8705a73c24a6ea7545a6
Elmar Ludwig <elmar.ludwig@uos.de>
2011-01-20 10:21:49 +0000

    Merged revisions 18446,18449,18455 via svnmerge from svn://develop.studip.de/studip/trunk
    ........
      r18446 | aklassen | 2011-01-11 16:24:00 +0100 (Tue, 11 Jan 2011) | 1 line
      
      refine error_message. give detailed information when something is wrong with a connection refs #1090
    ........
      r18449 | anoack | 2011-01-11 17:03:58 +0100 (Tue, 11 Jan 2011) | 1 line
      
       re #1090 
    ........
      r18455 | aklassen | 2011-01-12 14:45:42 +0100 (Wed, 12 Jan 2011) | 1 line
      
      remove TODO marker. Check seems plausible... fixes #1090
    ........
    
    
    git-svn-id: svn://develop.studip.de/studip/branches/1.11@18558 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- public/my_elearning.php
+++ public/my_elearning.php
@@ -72,14 +72,23 @@ if ($ELEARNING_INTERFACE_ENABLE)
 	if ($new_account_cms != "")
 		$new_account_form = ELearningUtils::getNewAccountForm($new_account_cms);
     foreach($ELEARNING_INTERFACE_MODULES as $cms => $cms_preferences) {
-		if (ELearningUtils::isCMSActive($cms) AND ($cms_preferences["auth_necessary"] == true))
+        if (ELearningUtils::isCMSActive($cms))
 		{
 			ELearningUtils::loadClass($cms);
-			ELearningUtils::bench("load cms $cms");
+            if ( $cms_preferences["auth_necessary"] == true) {
 			$new_module_form[$cms] = ELearningUtils::getNewModuleForm($cms);
-        } else {
-            $messages['error'] = sprintf(_("Das System %s ist momentan nicht erreichbar. Bitte wenden Sie sich an Ihren Systemadministrator."),$cms);
 		}
+            $connection_status = $connected_cms[$cms]->getConnectionStatus($cms);
+
+            foreach ($connection_status as $type => $msg)
+            {
+                if ($msg["error"] != "")
+                {
+                    $messages['error'] = sprintf(_("Es traten Probleme bei der Anbindung einzelner Lermodule auf. Bitte wenden Sie sich an Ihren Systemadministrator."),$cms);
+                    $errors[] = $msg['error'];
+    }
+            }
+        }
     }
 	if ($messages["info"] != "")
 	{
@@ -101,6 +110,8 @@ if ($ELEARNING_INTERFACE_ENABLE)
 	{
 		if (ELearningUtils::isCMSActive($cms))
 		{
+            $connected_cms = array();
+            ELearningUtils::loadClass($cms);
 			if (($cms_preferences["auth_necessary"] == true))
 			{
 				if ($GLOBALS["module_type_" . $cms] != "")
