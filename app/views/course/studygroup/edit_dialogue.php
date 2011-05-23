a3a31d15ab3b2878992d5fb8b364482a827bea19
Peter Thienel <thienel@data-quest.de>
2011-05-23 09:57:07 +0000

    Aufruf von CSRFProtection::Token() entfernt, fixes #1982 in source:branches/2.0
    git-svn-id: svn://develop.studip.de/studip/branches/2.0@20161 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- app/views/course/studygroup/edit_dialogue.php
+++ app/views/course/studygroup/edit_dialogue.php
@@ -4,7 +4,6 @@
                     . "\n" . $deactivate_modules_names) ?>
         <div style="margin-top: 1em;">
             <form action="<?= $controller->url_for('course/studygroup/update/'.$sem_id) ?>" method=post>
-                <?= CSRFProtection::tokenTag() ?>
                 <? foreach($this->flash['deactivate_modules'] as $module) :?>
                      <input type="hidden" name="deactivate_modules[]" value="<?=$module?>">
                 <? endforeach ?>
